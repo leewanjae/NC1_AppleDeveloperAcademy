2de13c47b14e2c0f1ae06644bcada3e52b42ac22
leewanjae <dhkswo1@gmail.com>
2024-04-08 23:02:29 +0900

    init

---
+++ TheManU/TheManUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheManUApp.swift
+//  TheManU
+//
+//  Created by LeeWanJae on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TheManUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
