08f1a801e5daa8435b6cb811116a118d1afa151b
leewanjae <dhkswo1@gmail.com>
2024-04-09 15:27:06 +0900

    feature: SplashView

---
+++ TheManU/View/MyApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyApp.swift
+//  TheManU
+//
+//  Created by LeeWanJae on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
---
+++ TheManU/View/SplashView.swift
@@ -0,0 +1,34 @@
+//
+//  SplashView.swift
+//  TheManU
+//
+//  Created by LeeWanJae on 4/9/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+    
+    var body: some View {
+        if isActive {
+            MatchesView()
+        } else {
+            Image("splash")
+                .resizable()
+                .ignoresSafeArea()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation{
+                            isActive = true
+                        }
+                    }
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    SplashView()
+}
